76aed72b6870849ad408b5dd0723db9984cf2f17
Austin M. Matherne <AustinMatherne@ProtonMail.ch>
2017-02-12 14:37:52 -0600

    Update theme for awesome 4
    Signed-off-by: Austin M. Matherne <AustinMatherne@ProtonMail.ch>

--- .config/awesome/themes/pango/theme.lua
+++ .config/awesome/themes/pango/theme.lua
@@ -3,34 +3,35 @@
 --         Pango         --
 ---------------------------
 
-theme = {}
+local theme = {}
 
---theme.font          = "sans 8"
+-- theme.font          = "sans 8"
 theme.font          = "Source Sans Pro 12"
 
 theme.bg_normal     = "#222222"
---theme.bg_focus      = "#535d6c"
+-- theme.bg_focus      = "#535d6c"
 theme.bg_focus      = "#3b3b3b"
---theme.bg_urgent     = "#ff0000"
+-- theme.bg_urgent     = "#ff0000"
 theme.bg_urgent     = "#5f5f5f"
---theme.bg_minimize   = "#444444"
+-- theme.bg_minimize   = "#444444"
 theme.bg_minimize   = "#1c1c1c"
 theme.bg_systray    = theme.bg_normal
 
---theme.fg_normal     = "#aaaaaa"
+-- theme.fg_normal     = "#aaaaaa"
 theme.fg_normal     = "#949494"
---theme.fg_focus      = "#ffffff"
+-- theme.fg_focus      = "#ffffff"
 theme.fg_focus      = "#d4d4d4"
---theme.fg_urgent     = "#ffffff"
+-- theme.fg_urgent     = "#ffffff"
 theme.fg_urgent     = "#d4d4d4"
---theme.fg_minimize   = "#ffffff"
+-- theme.fg_minimize   = "#ffffff"
 theme.fg_minimize   = "#d4d4d4"
 
+theme.useless_gap   = 0
 theme.border_width  = 1
 theme.border_normal = "#000000"
---theme.border_focus  = "#535d6c"
+-- theme.border_focus  = "#535d6c"
 theme.border_focus  = "#2b2b2b"
---theme.border_marked = "#91231c"
+-- theme.border_marked = "#91231c"
 theme.border_marked = "#202020"
 
 -- There are other variable sets
@@ -64,6 +65,9 @@ theme.menu_width  = 100
 theme.titlebar_close_button_normal = "/usr/share/awesome/themes/default/titlebar/close_normal.png"
 theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/default/titlebar/close_focus.png"
 
+theme.titlebar_minimize_button_normal = "/usr/share/awesome/themes/default/titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus  = "/usr/share/awesome/themes/default/titlebar/minimize_focus.png"
+
 theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/default/titlebar/ontop_normal_inactive.png"
 theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/default/titlebar/ontop_focus_inactive.png"
 theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/default/titlebar/ontop_normal_active.png"
@@ -84,7 +88,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
---theme.wallpaper = "/usr/share/awesome/themes/default/background.png"
+-- theme.wallpaper = "/usr/share/awesome/themes/default/background.png"
 theme.wallpaper = "~/.background.jpg"
 
 -- You can use your own layout icons like this:
@@ -100,12 +104,17 @@ theme.layout_tile = "/usr/share/awesome/themes/default/layouts/tilew.png"
 theme.layout_tiletop = "/usr/share/awesome/themes/default/layouts/tiletopw.png"
 theme.layout_spiral  = "/usr/share/awesome/themes/default/layouts/spiralw.png"
 theme.layout_dwindle = "/usr/share/awesome/themes/default/layouts/dwindlew.png"
+theme.layout_cornernw = "/usr/share/awesome/themes/default/layouts/cornernww.png"
+theme.layout_cornerne = "/usr/share/awesome/themes/default/layouts/cornernew.png"
+theme.layout_cornersw = "/usr/share/awesome/themes/default/layouts/cornersww.png"
+theme.layout_cornerse = "/usr/share/awesome/themes/default/layouts/cornersew.png"
 
 theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
--- Define the icon theme for application icons. If not set then the icons 
+-- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
 theme.icon_theme = nil
 
 return theme
+
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
